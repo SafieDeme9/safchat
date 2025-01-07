97027a00b98708b197ed0b5a011af65c4a9aa919
SafieDeme9 <safiedeme9@gmail.com>
2025-01-07 23:23:23 +0100

    added help command

--- main.py
+++ main.py
@@ -12,28 +12,31 @@ TOKEN = os.getenv("bot_token")
 if not TOKEN:
     raise ValueError("The bot token is not defined.")
 
-CHOICE = range(1)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Starts the conversation and asks the user about what they want to do."""
-    reply_keyboard = [["Translation", "Grammar correction", "Conversation"]]
 
     await update.message.reply_text(
-        "Hi! My name is Safchat. I will help you learn italian by holding a conversation with you, translate or correct your grammar. "
+        "Hi! My name is Safchat I am an LLM powered chatbot. I will help you learn italian by holding a conversation with you or translate. \n"
+        "Send /translate to translate in italian, /chat to hold a conversation with the bot and /help to get a help.\n"
         "Send /cancel to stop talking to me.\n\n"
-        "What are we going to do?",
-        reply_markup=ReplyKeyboardMarkup(
-            reply_keyboard, one_time_keyboard=True, input_field_placeholder="translation, grammar correction, conversation"
-        ),
+        "What are we going to do?"
     )
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Give the user the commands"""
 
-    return CHOICE
-
+    await update.message.reply_text(
+        "Send /translate to translate in italian, /chat to hold a conversation with the bot and /help to get a help.\n"
+        "Send /cancel to stop talking to me.\n\n"
+        "What are we going to do?"
+    )
 
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
+    application.add_handler(CommandHandler("help", help))
+
     
     application.run_polling()
