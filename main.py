b29e46a5f0f04a6b89b762a8af792f1c77e5ca36
SafieDeme9 <safiedeme9@gmail.com>
2025-01-08 20:46:35 +0100

    added cancel command

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import os
 import logging
 from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler, filters
 from transformers import pipeline
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     level=logging.INFO
 )
+logger = logging.getLogger(__name__)
 
 TOKEN = os.getenv("bot_token")
 if not TOKEN:
@@ -20,17 +21,27 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
     await update.message.reply_text(
         "Hi! My name is Safchat I am an LLM powered chatbot. I will help you learn italian by holding a conversation with you or translate. \n"
-        "Send /translate to translate in italian, /chat to hold a conversation with the bot and /help to get a help.\n"
+        "Send /translate + text to translate from english to italian,\n" 
+        "/chat to hold a conversation with the bot and\n"
+        "/help to get a help.\n"
         "Send /cancel to stop talking to me.\n\n"
         "What are we going to do?"
     )
 async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    """Give the user the commands"""
+    """Explains to the user the commands"""
 
     await update.message.reply_text(
         "Send /translate to translate in italian, /chat to hold a conversation with the bot and /help to get a help.\n"
         "Send /cancel to stop talking to me.\n\n"
         "What are we going to do?"
+    )
+
+async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Cancels and ends the conversation."""
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    await update.message.reply_text(
+        "Good bye and thanks for using Safchat!"
     )
 
 async def translate(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -46,13 +57,14 @@ async def translate(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     else:
         await update.message.reply_text("Please provide the text to translate. Example:\n/translate Hello, how are you?")
 
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
     application.add_handler(CommandHandler("help", help))
+    application.add_handler(CommandHandler("cancel", cancel))
     application.add_handler(CommandHandler("translate", translate))
-
     
     application.run_polling()
