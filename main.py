6dc6a246ca6e2c03d001bd0ac5a5382b505ddd9a
SafieDeme9 <safiedeme9@gmail.com>
2025-01-08 17:32:00 +0100

    added translation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import logging
 from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from transformers import pipeline
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -12,6 +13,7 @@ TOKEN = os.getenv("bot_token")
 if not TOKEN:
     raise ValueError("The bot token is not defined.")
 
+translator = pipeline("translation", model="Helsinki-NLP/opus-mt-en-it")
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Starts the conversation and asks the user about what they want to do."""
@@ -31,12 +33,26 @@ async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         "What are we going to do?"
     )
 
+async def translate(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Translate English text to Italian."""
+    if context.args:
+        input_text = " ".join(context.args)
+        try:
+            translated = translator(input_text)
+            translated_text = translated[0]['translation_text']
+            await update.message.reply_text(f"Translated text: {translated_text}")
+        except Exception as e:
+            await update.message.reply_text(f"An error occurred during translation: {e}")
+    else:
+        await update.message.reply_text("Please provide the text to translate. Example:\n/translate Hello, how are you?")
+
 if __name__ == '__main__':
     application = ApplicationBuilder().token(TOKEN).build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)
     application.add_handler(CommandHandler("help", help))
+    application.add_handler(CommandHandler("translate", translate))
 
     
     application.run_polling()
